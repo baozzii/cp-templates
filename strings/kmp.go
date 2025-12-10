f7f0bb8b72cba6a493f13893797103a45020d3f0
baozzii <yuhangliu1224@gmail.com>
2025-12-11 02:13:06 +0800

    add kmp

---
+++ strings/kmp.go
@@ -0,0 +1,17 @@
+package strings
+
+func GetPi[T comparable](s []T) []int {
+	n := len(s)
+	pi := make([]int, n)
+	for i := 1; i < n; i++ {
+		j := pi[i-1]
+		for j > 0 && s[i] != s[j] {
+			j = pi[j-1]
+		}
+		if s[i] == s[j] {
+			j++
+		}
+		pi[i] = j
+	}
+	return pi
+}
