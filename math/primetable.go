e2d9fe7878c1fc96eb2fe076fa1ccfc1a0f0657f
baozzii <yuhangliu1224@gmail.com>
2025-12-16 16:30:35 +0800

    optimise memory for primetable

--- math/primetable.go
+++ math/primetable.go
@@ -1,24 +1,24 @@
 package math
 
 type PrimeTable struct {
-	mpf []int
-	mu  []int
-	phi []int
-	pr  []int
+	mpf []int32
+	mu  []int8
+	phi []int32
+	pr  []int32
 	n   int
 }
 
 func NewPrimeTable(n int) *PrimeTable {
-	mpf := make([]int, n+1)
-	mu := make([]int, n+1)
-	phi := make([]int, n+1)
-	primes := make([]int, 0)
+	mpf := make([]int32, n+1)
+	mu := make([]int8, n+1)
+	phi := make([]int32, n+1)
+	primes := make([]int32, 0)
 	mu[1] = 1
 	phi[1] = 1
 	if n >= 1 {
 		mpf[1] = 1
 	}
-	for i := 2; i <= n; i++ {
+	for i := int32(2); i <= int32(n); i++ {
 		if mpf[i] == 0 {
 			mpf[i] = i
 			primes = append(primes, i)
@@ -26,7 +26,7 @@ func NewPrimeTable(n int) *PrimeTable {
 			phi[i] = i - 1
 		}
 		for _, p := range primes {
-			if p*i > n {
+			if p*i > int32(n) {
 				break
 			}
 			mpf[p*i] = p
@@ -44,7 +44,7 @@ func NewPrimeTable(n int) *PrimeTable {
 }
 
 func (pt *PrimeTable) IsPrime(x int) bool {
-	return pt.mpf[x] == x && x >= 2
+	return pt.mpf[x] == int32(x) && x >= 2
 }
 
 func (pt *PrimeTable) Factorize(x int) []int {
@@ -53,20 +53,8 @@ func (pt *PrimeTable) Factorize(x int) []int {
 	}
 	factors := make([]int, 0)
 	for x > 1 {
-		factors = append(factors, pt.mpf[x])
-		x /= pt.mpf[x]
+		factors = append(factors, int(pt.mpf[x]))
+		x /= int(pt.mpf[x])
 	}
 	return factors
-}
-
-func (pt *PrimeTable) Get(i int) int {
-	return pt.pr[i]
-}
-
-func (pt *PrimeTable) Phi(x int) int {
-	return pt.phi[x]
-}
-
-func (pt *PrimeTable) Mu(x int) int {
-	return pt.mu[x]
 }
