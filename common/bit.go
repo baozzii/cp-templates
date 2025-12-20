58582318044b09ccc1b5ba826f3a5243c2a947a5
baozzii <yuhangliu1224@gmail.com>
2025-12-21 01:40:17 +0800

    add lowbit highbit

--- common/bit.go
+++ common/bit.go
@@ -13,3 +13,15 @@ func Clz[T Integer](x T) int {
 func Popcount[T Integer](x T) int {
 	return bits.OnesCount(uint(x))
 }
+
+func Lowbit[T Integer](x T) T {
+	y := uint(x)
+	return T(y & -y)
+}
+
+func Highbit[T Integer](x T) T {
+	if x == 0 {
+		return x
+	}
+	return T(1) << (63 - Clz(uint(x)))
+}
