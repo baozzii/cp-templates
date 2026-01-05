555ef2ddb59912329f77b093aac1861eb7299f58
baozzii <yuhangliu1224@gmail.com>
2026-01-05 12:54:43 +0800

    implement with int32

--- datastructures/rollback_dsu.go
+++ datastructures/rollback_dsu.go
@@ -1,40 +1,41 @@
 package datastructures
 
 type RollbackDSU struct {
-	fa, sz []int
-	st     [][2]int
+	fa, sz []int32
+	st     [][2]int32
 }
 
 func NewRollbackDSU(n int) *RollbackDSU {
-	fa := make([]int, n)
-	sz := make([]int, n)
+	fa := make([]int32, n)
+	sz := make([]int32, n)
 	for i := range fa {
-		fa[i] = i
+		fa[i] = int32(i)
 		sz[i] = 1
 	}
-	return &RollbackDSU{fa, sz, make([][2]int, 0)}
+	return &RollbackDSU{fa, sz, make([][2]int32, 0)}
 }
 
-func (dsu *RollbackDSU) Find(u int) int {
-	if u == dsu.fa[u] {
-		return u
+func (dsu *RollbackDSU) Find(_u int) int {
+	u := int32(_u)
+	for u != dsu.fa[u] {
+		u = dsu.fa[u]
 	}
-	return dsu.Find(dsu.fa[u])
+	return int(u)
 }
 
 func (dsu *RollbackDSU) Merge(u, v int) bool {
 	u = dsu.Find(u)
 	v = dsu.Find(v)
 	if u == v {
-		dsu.st = append(dsu.st, [2]int{-1, -1})
+		dsu.st = append(dsu.st, [2]int32{-1, -1})
 		return false
 	}
 	if dsu.sz[u] > dsu.sz[v] {
 		u, v = v, u
 	}
 	dsu.sz[v] += dsu.sz[u]
-	dsu.fa[u] = v
-	dsu.st = append(dsu.st, [2]int{u, v})
+	dsu.fa[u] = int32(v)
+	dsu.st = append(dsu.st, [2]int32{int32(u), int32(v)})
 	return true
 }
 
